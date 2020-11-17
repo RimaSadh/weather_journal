4e2314dac1e8f4261d1bae36021a08b95f6ef9d4
Rima AlSadhan <44276239+RimaSadh@users.noreply.github.com>
2020-11-17 20:54:15 +0300

    Configure and define route methods

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+var projectData = {
+    temperature: '',
+    date: '',
+    user_response: '' 
+};
 
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
-const server = express();
+const app = express();
+
 
 /* Dependencies */
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+
+/* Middleware */
+// Configure express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -21,5 +28,30 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = process.env.port || 8080;
+const server = app.listen(port, () => {
+    console.log('Server is running on local host port: ', port);
+});
+
+
+/* ----SET ROUTES---- */
+// POST Route (adds incoming data to projectData)
+app.post('/postWeather', (req, res) => {
+
+    let newData = req.body;
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+    console.log("post wather function " + newData);
+
+    projectData.date = newDate;
+    projectData.temperature = newData.cod;
+    projectData.user_response = newData.name;
+
+});
+
+// GET Route (returns the projectData object)
+app.get('/all', (req, res) => { 
+    res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -3,3 +3,127 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Set API Credintials
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=ca671f7f06c4ee24ea9de753e9804d8b';
+
+
+const performAction = (e) => {
+
+    // Hide Error Message
+    document.getElementById('error').style.display = 'none';
+
+    // Get zip code entered by user, and perform validation
+    let zipCode = document.getElementById('zip').value;
+
+    if(zipCode.length != 0){ // Check it is not empty
+
+        getWeatherData(zipCode)
+        .then( (data) => {
+            
+            console.log("POST start"); // JSON
+            postWeatherData('http://localhost:8080/postWeather', data);
+            console.log("POST end");
+
+        })
+        .then( () => {
+            updateUI();
+        })
+        
+    }else{
+        // Zipcode field is empty --> Show Error Message
+        document.getElementById('error').style.display = 'block';
+    }
+}
+
+// Create event listener
+const generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', performAction);
+
+
+// Async GET data from API
+const getWeatherData = async(zipCode) => {
+
+    const response = await fetch(baseURL+zipCode+apiKey);
+
+    try{
+
+        const data = await response.json();
+        console.log("API GET RESPONSE: ", data);
+        return data;
+
+    }catch(error){
+        console.log("Error (getWeatherData): ", error);
+    }
+
+}
+
+// Async POST response to server 
+const postWeatherData = async(url, data) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+
+    try{
+        const newData = await response.json();
+        console.log("API POST DONE", newData);
+        return newData;
+
+    }catch(error){
+        console.log("Error (postWeatherData): ", error);
+    }
+
+}
+
+
+// Function to update the UI and render the response
+const updateUI = async() => {
+    
+    console.log("UPDATE UI start");
+
+    getAllData('http://localhost:8080/all')
+    
+    .then( (allData) => {
+
+        let date = document.getElementById('date');
+        let temp = document.getElementById('temp');
+        let content = document.getElementById('content');
+    
+        console.log("UPDATE UI MED : " + allData );
+    
+        // TODO: Rendered JSON data
+        date.innerHTML = allData.date;
+        temp.innerHTML = allData.temperature;
+        content.innerHTML = allData.user_response;
+    })
+
+}
+
+
+// GET request the projectData object 
+const getAllData = async(url) => {
+
+    const response = await fetch(url);
+
+    try{
+
+        const allData = await response.json();
+
+        console.log("ALL APP function " + allData.temperature);
+
+        return allData;
+
+    }catch(error){
+        console.log("Error (updateUI): ", error);
+    }
+
+}
+
+// Create UI element
